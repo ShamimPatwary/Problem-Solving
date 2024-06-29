9491a773e7e37c823c2fa3011db7e8dbee31dd95
ShamimPatwary <shamimpatwary002@gmail.com>
2024-06-29 14:16:03 +0600

    1008

---
+++ Beecrowd/1008.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n; //number
+    scanf("%d",&n);
+    int x;
+    scanf("%d",&x);
+    float y;
+    scanf("%f",&y);
+    float z;
+    z = (x*y);
+    printf("NUMBER = %d\n",n);
+    printf("SALARY = U$ %.2f\n",z);
+    return 0;
+
+}
