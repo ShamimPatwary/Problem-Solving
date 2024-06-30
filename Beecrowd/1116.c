2da7bd71cf5084649a9b69e3260257fee388f2e3
ShamimPatwary <shamimpatwary002@gmail.com>
2024-06-30 20:01:39 +0600

    1116

---
+++ Beecrowd/1116.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,y;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d %d",&x,&y);
+
+        if(y==0)
+        {
+            printf("divisao impossivel\n");
+        }
+        else
+        {
+            printf("%.1f\n",(float)x/y);
+        }
+    }
+    return 0;
+}
