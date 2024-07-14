9a053007cd71a599fb023c14bd02ca56ca8c89f3
ShamimPatwary <shamimpatwary002@gmail.com>
2024-07-14 21:54:06 +0600

    1190

---
+++ Beecrowd/1190.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    double M[12][12],sum=0.0;
+    char x[2];
+    scanf("%c",&x);
+    int n=5,m=6;
+    for(int i=0;i<12;i++)
+    {
+        for(int j=0;j<12;j++)
+        {
+            scanf("%lf",&M[i][j]);
+        }
+    }
+    for(int i=7;i<12;i++)
+    {
+        for(int j=n;j<=m;j++)
+        {
+            sum+=M[j][i];
+        }
+        n--;
+        m++;
+    }
+    if(x[0]=='S') printf("%.1lf\n",sum);
+    else if(x[0]=='M') printf("%.1lf\n",sum/30.0);
+    return 0;
+}
+
