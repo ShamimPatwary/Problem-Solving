e188ce3c34f7894f47eecb45481b07a1c43d2258
ShamimPatwary <shamimpatwary002@gmail.com>
2024-07-16 18:08:40 +0600

    1541

---
+++ Beecrowd/1541.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i;
+    double area;
+    for(;;)
+    {
+        scanf("%d",&a);
+        if(a==0)break;
+        scanf("%d %d",&b,&c);
+        area = a*b*100.0/c;
+        for(i=1;i*i<=area;i++);
+        i--;
+        printf("%d\n",i);
+    }
+    return 0;
+}
